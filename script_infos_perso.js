5da5179673c56fe26c688f612cab10faadeb661f
Sacha6254 <sachagoulon@gmail.com>
2025-03-04 13:30:58 +0100

    remplir les buts

--- script_infos_perso.js
+++ script_infos_perso.js
@@ -2,7 +2,7 @@ document.getElementById('but1').addEventListener('click', function() {
     document.getElementById('personal-info-modal').style.display = 'block';
 });
 
-document.getElementsByClassName('close-button')[0].addEventListener('click', function() {
+document.getElementsByClassName('close-button1')[0].addEventListener('click', function() {
     document.getElementById('personal-info-modal').style.display = 'none';
 });
 
@@ -13,46 +13,16 @@ window.addEventListener('click', function(event) {
     }
 });
 
-document.getElementById('but1').addEventListener('click', function() {
-    document.getElementById('personal-info-modal').style.display = 'block';
+document.getElementById('but2').addEventListener('click', function() {
+    document.getElementById('cv-modal').style.display = 'block';
 });
 
-document.getElementsByClassName('close-button')[0].addEventListener('click', function() {
-    document.getElementById('personal-info-modal').style.display = 'none';
-});
-
-window.addEventListener('click', function(event) {
-    var modal = document.getElementById('personal-info-modal');
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
-});
-
-document.getElementById('but1').addEventListener('click', function() {
-    document.getElementById('personal-info-modal').style.display = 'block';
-});
-
-document.getElementsByClassName('close-button')[0].addEventListener('click', function() {
-    document.getElementById('personal-info-modal').style.display = 'none';
+document.getElementsByClassName('close-button2')[0].addEventListener('click', function() {
+    document.getElementById('cv-modal').style.display = 'none';
 });
 
 window.addEventListener('click', function(event) {
-    var modal = document.getElementById('personal-info-modal');
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
-});
-
-document.getElementById('but1').addEventListener('click', function() {
-    document.getElementById('personal-info-modal').style.display = 'block';
-});
-
-document.getElementsByClassName('close-button')[0].addEventListener('click', function() {
-    document.getElementById('personal-info-modal').style.display = 'none';
-});
-
-window.addEventListener('click', function(event) {
-    var modal = document.getElementById('personal-info-modal');
+    var modal = document.getElementById('cv-modal');
     if (event.target == modal) {
         modal.style.display = 'none';
     }
